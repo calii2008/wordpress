2f30a9c7e4dc60152dac3e52f1f601fad3277971
calii2008 <ensarsahiti60@gmail.com>
2025-06-19 07:05:39 -0700

    calyy

---
+++ functions.php
@@ -0,0 +1,99 @@
+<?php
+
+function load_scripts(){
+    
+    wp_enqueue_style(
+
+        'style',
+        get_stylesheet_uri(),
+        array(),
+        filemtime(get_template_directory(). '/style.css'), 'all'
+        );
+        wp_enqueue_script('dropdown',get_template_directory(). '/js/dropdown.js',array(),'1.0',false);
+}
+
+add_action('wp_enqueue_scripts','load_scripts');
+
+function config(){
+    register_nav_menus(
+        array(
+            'wp_devs_menu' => 'main_menu',
+            'wp_devs_footer_menu' => 'footer_menu'
+        )
+    );
+    $args=array(
+        'height' => 225,
+        'width' => 1920
+    );
+    add_theme_support('custom-header', $args);
+    add_theme_support('post-thumbnails');
+    add_theme_support('costum-logo', array(
+        'height' => 200,
+        'width' => 110,
+        'flex-height' => true,
+        'flex-width' => true
+    ));
+    add_theme_support('automatic-feed-links');
+    add_theme_support('html5', array('comment-list','comment-form','search-form','gallery','caption','style','script'));
+    add_theme_support('title-tag');
+
+
+}
+
+add_action('after_setup_theme','config');
+
+funcrion wp_devs_widgets_init() {
+    register_sidebar(
+        array(
+            'name' => 'Sidebar',
+            'id' => 'sidebar-1',
+            'description' => 'Main Sidebar',
+            'before_widget' => 'this is the blog sibebar.you can add widgets here',
+            'after_widget' => '</div>',
+            'before_title' => '<h2 class="widget-title">',
+            'after_title' => '</h2>'
+            
+        )
+    );
+    register_sidebar(
+        array(
+            'name' => 'services 1',
+            'id' => 'service-1',
+            'description' => 'Footer Service area',
+            'before_widget' => '<div class="footer-widget">',
+            'after_widget' => '</div>',
+            'before_title' => '<h4 class="widget-title">',
+            'after_title' => '</h4>'
+        )
+    );
+
+register_sidebar(
+        array(
+            'name' => 'services 2',
+            'id' => 'service-2',
+            'description' => 'Footer Service area',
+            'before_widget' => '<div class="footer-widget">',
+            'after_widget' => '</div>',
+            'before_title' => '<h4 class="widget-title">',
+            'after_title' => '</h4>'
+        )
+    );
+
+register_sidebar(
+        array(
+            'name' => 'services 3',
+            'id' => 'service-3',
+            'description' => 'Footer Service area',
+            'before_widget' => '<div class="footer-widget">',
+            'after_widget' => '</div>',
+            'before_title' => '<h4 class="widget-title">',
+            'after_title' => '</h4>'
+        )
+    );
+
+
+
+}
+add_action('widgets_init','wp_devs_sidebars');
+
+?>
